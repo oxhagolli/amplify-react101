2462cbf0b7a0a6e967daf8419dbb04306f9c3580
Orens Xhagolli <orxhagol@microsoft.com>
2020-06-19 09:30:02 -0700

    Change binding strategy, add minor cosmetic fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
+import "./ToDo";
 
 class TodoApp extends React.Component {
+  // Don't use class variables other than state, props
+
+  // You can also just say state = {items: [], text: "" }
   constructor(props) {
     super(props);
     this.state = { items: [], text: "" };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   render() {
@@ -21,6 +23,7 @@ class TodoApp extends React.Component {
             id="new-todo"
             onChange={this.handleChange}
             value={this.state.text}
+            name="text"
           />
           <button>Add #{this.state.items.length + 1}</button>
         </form>
@@ -28,12 +31,13 @@ class TodoApp extends React.Component {
     );
   }
 
-  handleChange(e) {
-    this.setState({ text: e.target.value });
-  }
+  handleChange = (e) => {
+    // Always use set state
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-  handleSubmit(e) {
-    e.preventDefault();
+  handleSubmit = (e) => {
+    e.preventDefault(); // Stop form submit
     if (this.state.text.length === 0) {
       return;
     }
@@ -45,19 +49,7 @@ class TodoApp extends React.Component {
       items: state.items.concat(newItem),
       text: "",
     }));
-  }
-}
-
-class TodoList extends React.Component {
-  render() {
-    return (
-      <ul>
-        {this.props.items.map((item) => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
-    );
-  }
+  };
 }
 
 export default TodoApp;
